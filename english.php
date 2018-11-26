da9383dce6f4a84a1ee1964d909ea0f1b9f49647
Willem van der Plaat <wplaat@gmail.com>
2018-11-26 17:21:21 +0100

    Update copyright banner

--- english.php
+++ english.php
@@ -13,7 +13,7 @@
 **  Or send an email to the following address.
 **  Email   : info@plaatsoft.nl
 **
-**  All copyrights reserved (c) 2008-2016 PlaatSoft
+**  All copyrights reserved (c) 1996-2016 PlaatSoft
 */
 
 /**
@@ -28,7 +28,7 @@
 */
 
 $lang['TITLE'] = 'PlaatOnline';
-$lang['LINK_COPYRIGHT'] = '<a class="normal_link" href="http://www.plaatsoft.nl/">PlaatSoft</a> 2008-'.date("Y").' - All Copyright Reserved ';
+$lang['LINK_COPYRIGHT'] = '<a class="normal_link" href="http://www.plaatsoft.nl/">PlaatSoft</a> 1996-'.date("Y").' - All Copyright Reserved ';
 
 $lang['GENERAL_WARNING'] = 'WARNING';
 
@@ -37,7 +37,7 @@ Rename config.php.sample to config.php, update the database settings en press F5
 
 $lang['DATABASE_CONNECTION_FAILED' ] = 'The connection to the database failed. Please check if config.php settings!';
 
-$lang['COPYRIGHT'] = 'All Copyright Reserved &copy; <a class="normal_link" href="http://www.plaatsoft.nl/">PlaatSoft</a> 2008 - ' . date("Y");
+$lang['COPYRIGHT'] = 'All Copyright Reserved &copy; <a class="normal_link" href="http://www.plaatsoft.nl/">PlaatSoft</a> 1996 - ' . date("Y");
 
 
 /*
